62cd78863e104bebd987111096f17b8cad6eebd0
mayaams <mayaams27@gmail.com>
2019-11-19 23:44:46 +0700

    add product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="my-8 ma-3 flat text-xs-center" max-width="315">
+    <v-img :src="`${productCard.image}`" aspect- height="315"></v-img>
+    <v-card-title>
+      {{ productCard.name }}
+    </v-card-title>
+    <v-card-text>
+      <v-btn depressed small class="mx-auto white--text">
+        {{ productCard.division }}
+      </v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["productCard"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.v-card
+  font-family: $raleway
+  border-radius: 25px
+
+.v-btn
+  background: $orange-gradient
+  border-radius: 11px
+
+.v-card-title
+  font-size: 28px
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,98 @@
 <template>
-  <div class="about">
-    <h1>This is a product page</h1>
+  <div class="product">
+    <v-container class="px-sm-6 mt-12">
+      <v-layout row wrap>
+        <v-flex
+          xs12
+          sm6
+          md4
+          v-for="productCard in productList"
+          :key="productCard.id"
+        >
+          <ProductCards :productCard="productCard" />
+        </v-flex>
+        <v-btn depressed class="mx-auto white--text">
+          SEE MORE
+        </v-btn>
+      </v-layout>
+    </v-container>
+    <Footer />
   </div>
 </template>
+
+<script>
+import ProductCards from "@/components/ProductCard";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    ProductCards,
+    Footer
+  },
+  data() {
+    return {
+      productList: [
+        {
+          id: 1,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 2,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 3,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 4,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 5,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 6,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 7,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 8,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        },
+        {
+          id: 9,
+          name: "KATSIR",
+          image: require("@/assets/product/product_mobile.png"),
+          division: "MOBILE"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.v-btn
+  background: $orange-gradient
+  font-family: $raleway
+</style>
